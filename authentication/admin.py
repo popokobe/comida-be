c8b198959be491a1e900c99840c25cd5aa9eb873
popokobe <yuki.n3kobe@gmail.com>
2020-03-15 22:06:34 +0900

    管理画面でアカウントを管理可能に

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,1 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserChangeForm, UserCreationForm
+from .models import Account
+
+
+class AccountAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_admin',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.unregister(Group)
