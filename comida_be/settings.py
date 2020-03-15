b5bee3fa52a342f400ad7ab23cbb699ea8b722b5
popokobe <yuki.n3kobe@gmail.com>
2020-03-15 19:04:11 +0900

    /loginにusernameとpasswordを投げると,tokenを返すようにした

--- comida_be/settings.py
+++ comida_be/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apiv0',
+    'authentication',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +71,22 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'comida_be.wsgi.application'
+
+JWT_AUTH = {
+    'JWT_VERIFY_EXPIRATION': False,
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'NON_FIELD_ERRORS_KEY': 'detail',
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
 
 
 # Database
