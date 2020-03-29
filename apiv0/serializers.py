efa82feb8288675ed6b5da2fee2fb41e2d5d8af7
popokobe <yuki.n3kobe@gmail.com>
2020-03-29 13:42:51 +0900

    GET時はネストしたauthorを、POST時はidを指定できるように

--- apiv0/serializers.py
+++ apiv0/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
-from authentication.serializers import AccountSerializer
-from django.conf import settings
 from .models import Post
+
+from authentication.serializers import AccountSerializer
+from authentication.models import Account
+
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = AccountSerializer(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(queryset=Account.objects.all(), write_only=True)
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['author', 'author_id', 'created_at', 'updated_at', 'name', 'area', 'address', 'dish', 'category', 'expense', 'note', 'rating']
+        read_only_fields = ['created_at', 'updated_at']
 
-    def to_representation(self, instance):
-        self.fields['author'] = AccountSerializer(read_only=True)
-        return super(PostSerializer, self).to_representation(instance)
+    def create(self, validated_data):
+        validated_data['author'] = validated_data.get('author_id', None)
+
+        if validated_data['author'] is None:
+            raise serializers.ValidationError("author not found.") 
+
+        del validated_data['author_id']
+
+        return Post.objects.create(**validated_data)
